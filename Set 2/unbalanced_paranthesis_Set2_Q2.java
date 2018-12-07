e73f4842fbac9c45add1c5f5e8c14b1a0b7578c5
Vishesh Sanghani <33822998+visheshsanghani@users.noreply.github.com>
2018-12-07 14:55:25 +0530

    Corrected Error

--- Set 2/unbalanced_paranthesis_Set2_Q2.java
+++ Set 2/unbalanced_paranthesis_Set2_Q2.java
@@ -3,7 +3,7 @@ package Set_1;
 public class unbalanced_paranthesis_Set2_Q2 {
 
 	public static void main(String[] args) {
-		String buffer="(a)()";
+		String buffer="))(a)))";
 		int open=0;
 		int[] check=new int[buffer.length()];
 		char[] input=buffer.toCharArray();
@@ -13,14 +13,14 @@ public class unbalanced_paranthesis_Set2_Q2 {
 				open++;
 			}
 			else if(input[i]==')'){
-				if(open!=0){
+				if(open>0&&input[check[open-1]]!=')'){
 				open--;
-			}else{
-				check[open]=i;
-				open++;
+				}
+					else{
+						check[open]=i;
+						open++;
 			}
-			
-		}
+				}
 	}
 		for(int iteration=0;iteration<input.length;iteration++){
 		     int flag=0;
@@ -29,8 +29,7 @@ public class unbalanced_paranthesis_Set2_Q2 {
 		     flag=1;
 		     if(flag==0)
 		    	 System.out.print(input[iteration]);
+			}
+		}
 
-
-}
-}
-}
+	}
